7f4391ee739cce05d7b574773d7c2de173d32077
Bareman07 <146845508+Bareman07@users.noreply.github.com>
2025-03-16 22:27:35 +0100

    addFirstContact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  nom: string = '';
+  prenom: string = '';
+  email: string = '';
+  telephone: string = '';
+  message: string = '';
 
+  // Fonction appelée à la soumission du formulaire
+  envoyerMessage() {
+    // Affiche les infos dans la console
+    console.log('Message envoyé :', {
+      nom: this.nom,
+      prenom: this.prenom,
+      email: this.email,
+      telephone: this.telephone,
+      message: this.message
+    });
+
+    // Réinitialiser le formulaire après envoi
+    this.nom = '';
+    this.prenom = '';
+    this.email = '';
+    this.telephone = '';
+    this.message = '';
+  }
 }
