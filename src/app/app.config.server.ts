3b27e1720fa8edca2eac1b5013a8931c0c785055
Bareman07 <146845508+Bareman07@users.noreply.github.com>
2025-03-26 12:29:13 +0100

    ModifHeader

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -3,6 +3,8 @@ import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { RechercheProduitPipe } from './pipes/recherche-produit.pipe';
+
 
 const serverConfig: ApplicationConfig = {
   providers: [
